67efd6ae62efb5ae90bac2e4c6293ae0cbd39e74
SamHuang <sam821203@yahoo.com.tw>
2023-10-10 22:31:49 +0800

    styling bar chart

--- browserdata.js
+++ browserdata.js
@@ -1,37 +1,38 @@
-    const browserData = [
-      { 
-        browser: 'Chrome', 
-        color: 'rgba(75, 192, 192, 1)',
-        users: 150,
-        marketshare: 70,
-        versionData: [
-          {version: 'v5', users: 10},
-          {version: 'v6', users: 20},
-          {version: 'v7', users: 30},
-          {version: 'v8', users: 60},
-          {version: 'v9', users: 20},
-        ],
-      },
-      { 
-        browser: 'FireFox',
-        color: 'rgba(255, 26, 104, 1)',
-        users: 25,
-        marketshare: 24,
-        versionData: [
-          {version: 'V3.1', users: 10},
-          {version: 'v3.2', users: 10},
-          {version: 'v3.3', users: 5},
-        ],
-      },    
-      { 
-        browser: 'Safari', 
-        color: 'rgba(54, 162, 235, 1)', 
-        users: 30,
-        marketshare: 26,
-        versionData: [
-          {version: 'Web 9', users: 10},
-          {version: 'Web 10', users: 10},
-          {version: 'Web 11', users: 10},
-        ],
-      }   
-    ];
+const browserData = [
+  {
+    browser: "One Piece",
+    color: "rgba(234, 115, 23, 0.3)",
+    popularity: 83720,
+    versionData: [
+      { version: "Episode 1", views: 24000 },
+      { version: "Episode 2", views: 20000 },
+      { version: "Episode 3", views: 13500 },
+      { version: "Episode 4", views: 9500 },
+      { version: "Episode 5", views: 16720 },
+    ],
+  },
+  {
+    browser: "Shingeki no Kyojin",
+    color: "rgba(255, 26, 104, 0.3)",
+    popularity: 54770,
+    versionData: [
+      { version: "Episode 1", views: 20000 },
+      { version: "Episode 2", views: 12000 },
+      { version: "Episode 3", views: 13500 },
+      { version: "Episode 4", views: 7600 },
+      { version: "Episode 5", views: 1670 },
+    ],
+  },
+  {
+    browser: "JUJUTSU KAISEN",
+    color: "rgba(54, 162, 235, 0.3)",
+    popularity: 80720,
+    versionData: [
+      { version: "Episode 1", views: 18000 },
+      { version: "Episode 2", views: 16000 },
+      { version: "Episode 3", views: 5000 },
+      { version: "Episode 4", views: 11000 },
+      { version: "Episode 5", views: 30720 },
+    ],
+  },
+];

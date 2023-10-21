b59b45d96168b0bf05358e8116af21a13a0ad9d3
SamHuang <sam821203@yahoo.com.tw>
2023-10-21 14:33:24 +0800

    docs: global temperture & get ISS

---
+++ charts-component/src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <canvas ref="myChart"></canvas>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import Chart from "chart.js/auto";
+
+const myChart = ref(null);
+
+onMounted(() => {
+  const ctx = myChart.value;
+
+  new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
